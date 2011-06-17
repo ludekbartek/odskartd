a70052d2df107916f965f861f806e32d026c7370
ludek.bartek@gmail.com <ludek.bartek@gmail.com@c602cd01-3548-1a7f-b3ab-c16ba7af9826>
2011-06-17 18:18:48 +0000

    Start of UI implementation.

---
+++ trunk/src/cz/muni/fi/bar/gui/TypeComboBoxModel.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.bar.gui;
+
+import cz.muni.fi.bar.odsdb.DBManager;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.AbstractListModel;
+import javax.swing.ComboBoxModel;
+import javax.swing.event.ListDataListener;
+
+/**
+ *
+ * @author bar
+ */
+class TypeComboBoxModel extends AbstractListModel{
+
+    private List<String> types = new ArrayList<String>();
+    private DBManager manager;
+
+    public TypeComboBoxModel() {
+        
+    }
+
+    public void setDBManager(DBManager manager){
+        this.manager = manager;
+        types.addAll(this.manager.getMediaTypes());
+    }
+    @Override
+    public int getSize() {
+        return types.size();
+    }
+
+    @Override
+    public Object getElementAt(int i) {
+        return types.get(i);
+    }
+
+}
