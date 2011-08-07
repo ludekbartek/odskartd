f893080d8405c93bdfff39f2e1db798f53409ca8
ludek.bartek@gmail.com <ludek.bartek@gmail.com@c602cd01-3548-1a7f-b3ab-c16ba7af9826>
2011-08-07 08:02:06 +0000

    Implemented adding new media and a new movie to a selected media.

--- trunk/src/cz/muni/fi/bar/gui/ODSKart.java
+++ trunk/src/cz/muni/fi/bar/gui/ODSKart.java
@@ -14,13 +14,19 @@ import cz.muni.fi.bar.odsdb.DBManager;
 import cz.muni.fi.bar.odsdb.ODSDBManager;
 import cz.muni.fi.bar.odsdb.ODSKartException;
 import cz.muni.fi.bar.odsdb.entities.Medium;
+import cz.muni.fi.bar.odsdb.entities.MediumImpl;
 import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.StreamHandler;
 import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.ComboBoxModel;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -32,6 +38,62 @@ import javax.swing.table.AbstractTableModel;
  */
 public class ODSKart extends javax.swing.JFrame {
 
+    private class AddMediumAction extends AbstractAction{
+
+        private javax.swing.JTable table;
+        
+        public AddMediumAction(String caption,javax.swing.JTable table){
+            super(caption);
+            this.table = table;
+        }
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            ODSKartTableModel model = (ODSKartTableModel) table.getModel();
+            model.addMedium();
+        }
+        
+    }
+    
+    private class AddMovieAction extends AbstractAction {
+
+        
+        
+        public AddMovieAction(String string) {
+            super(string);
+            
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            if(jTable1.getSelectedRows().length>0)
+            {
+                jDialog1.setVisible(true);
+                Medium medium = null;
+                if(filmOk){
+                    System.err.println("Pridavam film");
+                    ODSKartTableModel tableModel = (ODSKartTableModel) jTable1.getModel();
+                    try{
+                        int index = jTable1.getSelectedRow();
+                        medium = tableModel.getMedium(index);
+                    }catch(ODSKartException ex){
+                      logger.log(Level.SEVERE, "Nelze ziskat medium pro pridani filmu: {0}", ex);   
+                    }
+                    if(medium!=null){
+                        //jDialog1.setVisible(fal);
+                        medium.addTitle(jTextField1.getText());
+                        tableModel.updateView();
+                    }
+                }
+            }else{
+                jDialog2.setVisible(true);
+            }
+            
+        }
+
+        
+    }
+
+    private boolean filmOk = false;
     private class ComboBoxAction extends AbstractAction {
 
         public ComboBoxAction() {
@@ -43,7 +105,7 @@ public class ODSKart extends javax.swing.JFrame {
             String type = (String)jComboBox1.getSelectedItem();
             videotekModel.setType(type);
             videotekModel.reload();
-            System.err.println("Type:" + type);
+           // System.err.println("Type:" + type);
         }
     }
 
@@ -69,6 +131,8 @@ public class ODSKart extends javax.swing.JFrame {
                 ((TypeComboBoxModel)comboBoxModel1).setDBManager(manager);
                 videotekModel.setDataManager(manager);
                 jMenuItem1.setEnabled(true);
+                jMenu2.setEnabled(true);
+                jMenu3.setEnabled(true);
                 videotekModel.reload();
             }
         }
@@ -95,7 +159,7 @@ public class ODSKart extends javax.swing.JFrame {
         @Override
         public int getRowCount() {
             int rowCount = media.size();
-            System.err.println("Medii: " + rowCount);
+           // System.err.println("Medii: " + rowCount);
             return rowCount;
         }
 
@@ -167,6 +231,20 @@ public class ODSKart extends javax.swing.JFrame {
                 Logger.getLogger(ODSKart.class.getName()).log(Level.SEVERE, "Unable to set the title:", ex);
             }
         }
+        
+        public Medium getMedium(int index) throws ODSKartException{
+            if(index>media.size()||index<0)throw new ODSKartException("Invalid medium number "+index+". There are " + media.size()+" media.");
+            return media.get(index);
+        }
+
+        private void addMedium() {
+            media.add(new MediumImpl());
+            fireTableRowsInserted(0, media.size());
+        }
+
+        private void updateView() {
+            fireTableCellUpdated(0,media.size());
+        }
     }
     
     private class ExitAction extends AbstractAction{
@@ -182,11 +260,20 @@ public class ODSKart extends javax.swing.JFrame {
     private DBManager manager;
     private ComboBoxModel comboBoxModel1;
     private ODSKartTableModel videotekModel;
+    private Logger logger;
     /** Creates new form ODSKart */
     public ODSKart() {
         comboBoxModel1 = (ComboBoxModel) new TypeComboBoxModel();
         videotekModel = new ODSKartTableModel();
         initComponents();
+        try {
+            logger = Logger.getLogger(ODSKart.class.getName());
+            logger.addHandler(new StreamHandler(new FileOutputStream("ODSKart.log"),new SimpleFormatter()));
+            
+        } catch (FileNotFoundException ex) {
+            System.err.println("Exception while creating logger:"+ex);
+            logger = Logger.getAnonymousLogger();
+        }
         jTable1.setModel(videotekModel);
     }
 
@@ -200,6 +287,15 @@ public class ODSKart extends javax.swing.JFrame {
     private void initComponents() {
 
         jFileChooser1 = new javax.swing.JFileChooser();
+        jDialog1 = new javax.swing.JDialog();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jDialog2 = new javax.swing.JDialog();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jButton5 = new javax.swing.JButton();
         jToolBar1 = new javax.swing.JToolBar();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
@@ -214,6 +310,63 @@ public class ODSKart extends javax.swing.JFrame {
         jMenu2 = new javax.swing.JMenu();
         jMenuItem4 = new javax.swing.JMenuItem();
         jMenuItem5 = new javax.swing.JMenuItem();
+        jMenu3 = new javax.swing.JMenu();
+        jMenuItem6 = new javax.swing.JMenuItem();
+
+        jDialog1.setTitle("Přidání filmu");
+        jDialog1.setModal(true);
+        jDialog1.setName("filmDialog"); // NOI18N
+        jDialog1.getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jDialog1.setSize(jDialog1.getPreferredSize());
+        jDialog1.pack();
+
+        jLabel1.setText("Jméno filmu:");
+        jLabel1.setName("movieTitle"); // NOI18N
+        jDialog1.getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 26, -1, -1));
+
+        jTextField1.setColumns(25);
+        jTextField1.setName("movieTitleField"); // NOI18N
+        jDialog1.getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(114, 20, -1, -1));
+
+        jButton3.setText("Ok");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jDialog1.getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(448, 21, 86, -1));
+
+        jButton4.setText("Cancel");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        jDialog1.getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(448, 55, -1, -1));
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setText("Nejdříve je nutné zvolit médium.");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 11, -1, -1));
+
+        jButton5.setText("OK");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(126, 34, -1, -1));
+
+        org.jdesktop.layout.GroupLayout jDialog2Layout = new org.jdesktop.layout.GroupLayout(jDialog2.getContentPane());
+        jDialog2.getContentPane().setLayout(jDialog2Layout);
+        jDialog2Layout.setHorizontalGroup(
+            jDialog2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 321, Short.MAX_VALUE)
+        );
+        jDialog2Layout.setVerticalGroup(
+            jDialog2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 73, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -257,14 +410,24 @@ public class ODSKart extends javax.swing.JFrame {
         jMenuBar1.add(jMenu1);
 
         jMenu2.setText("Médium");
+        jMenu2.setEnabled(false);
 
-        jMenuItem4.setText("Přidat");
+        jMenuItem4.setAction(new AddMediumAction("Přidat",jTable1));
         jMenu2.add(jMenuItem4);
 
         jMenuItem5.setText("Změnit");
         jMenu2.add(jMenuItem5);
 
         jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Titul");
+        jMenu3.setEnabled(false);
+
+        jMenuItem6.setAction(new AddMovieAction("Přidat médium"));
+        jMenuItem6.setText("Přidat titul");
+        jMenu3.add(jMenuItem6);
+
+        jMenuBar1.add(jMenu3);
 
         setJMenuBar(jMenuBar1);
 
@@ -286,6 +449,21 @@ public class ODSKart extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        jDialog1.setVisible(false);
+        filmOk = true;
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        jDialog1.setVisible(false);
+        filmOk = false;
+   
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        jDialog2.setVisible(false);
+    }//GEN-LAST:event_jButton5ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -301,18 +479,29 @@ public class ODSKart extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
     private javax.swing.JComboBox jComboBox1;
+    private javax.swing.JDialog jDialog1;
+    private javax.swing.JDialog jDialog2;
     private javax.swing.JFileChooser jFileChooser1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JTextField jTextField1;
     private javax.swing.JToolBar jToolBar1;
     // End of variables declaration//GEN-END:variables
 }
